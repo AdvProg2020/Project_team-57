ce242392da1e9df87d812b8d5b9b129e1aa872ce
sepehrkianian09 <59197271+sepehrkianian09@users.noreply.github.com>
2020-05-02 06:58:06 +0430

    Finishing GetController of Control Classes

--- src/main/java/controller/account/Admin.java
+++ src/main/java/controller/account/Admin.java
@@ -3,7 +3,7 @@ package controller.account;
 public class Admin extends AccountControl{
     private static Admin adminControl = null;
 
-    public static AccountControl getController() {
+    public static Admin getController() {
         if (adminControl == null)
             adminControl = new Admin();
 
--- src/main/java/controller/account/Vendor.java
+++ src/main/java/controller/account/Vendor.java
@@ -3,7 +3,7 @@ package controller.account;
 public class Vendor extends AccountControl{
     private static Vendor vendorControl = null;
 
-    public static AccountControl getController() {
+    public static Vendor getController() {
         if (vendorControl == null)
             vendorControl = new Vendor();
 
