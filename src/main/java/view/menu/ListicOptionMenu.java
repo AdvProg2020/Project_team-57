2f01ce6cf25b78e198e1715055560283f22c95a4
sepehrkianian09 <59197271+sepehrkianian09@users.noreply.github.com>
2020-05-12 03:19:33 +0430

    Uniting all Execute Functions into one function

--- src/main/java/view/menu/ListicOptionMenu.java
+++ src/main/java/view/menu/ListicOptionMenu.java
@@ -2,6 +2,7 @@ package view.menu;
 
 import controller.Control;
 import view.PrintOptionSpecs;
+import view.process.Processor;
 import view.process.ProductProcessor;
 import view.process.UserProcessor;
 
@@ -53,7 +54,38 @@ public class ListicOptionMenu extends Menu implements PrintOptionSpecs {
 
     }
 
-    protected void printOptionSpecs(){}
+    public Menu execute(){
+        processor = Processor.findProcessorWithName(processorName);
+        Menu nextMenu = this;
+        boolean flag = true;
+        int input = 0;
+
+        while(flag){
+            try {
+                input = Integer.parseInt(scanner.nextLine().trim());
+
+                if (input > options.size() || input < 0)
+                    throw new InputIsBiggerThanExistingNumbers("Invalid Number!!! \nWhat are you doing, man?!");
+                else
+                    flag = false;
+
+            } catch (NumberFormatException e) {
+                System.out.println("Please Enter An Integer");
+            } catch (NullPointerException e) {
+                System.out.println("Please Enter An Integer");
+            } catch (InputIsBiggerThanExistingNumbers e) {
+                System.out.println(e.getMessage());
+            }
+        }
+
+        if(input == 0) {
+            nextMenu = parentMenu;
+        } else if(processor.isThereFunctionWithName(options.get(input - 1))) {
+            nextMenu = processor.executeTheFunctionWithName(options.get(input - 1), this, option);
+        }
+
+        return nextMenu;
+    }
 
     public void setParentMenu(Menu parentMenu) {
         this.parentMenu = parentMenu;
