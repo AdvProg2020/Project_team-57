812af9f5cc81dc427a3472fbc216aad046321912
sepehrkianian09 <59197271+sepehrkianian09@users.noreply.github.com>
2020-06-05 00:23:38 +0430

    Setting SignIn For View

---
+++ src/main/java/view/SignInProcessor.java
@@ -0,0 +1,40 @@
+package view;
+
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextField;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.paint.Color;
+
+import java.io.IOException;
+
+public class SignInProcessor {
+
+    public JFXTextField userNameField, passwordField;
+
+    public void changeSignUpColor(MouseEvent mouseEvent) {
+        JFXButton button = (JFXButton) mouseEvent.getSource();
+        button.setTextFill(Color.valueOf("#b0bec5"));
+    }
+
+    public void loginChangeColor(MouseEvent mouseEvent) {
+        JFXButton button = (JFXButton) mouseEvent.getSource();
+        button.setTextFill(Color.BLACK);
+    }
+
+    public void login(MouseEvent mouseEvent) {
+        //TODO
+    }
+
+
+    public void enterSignUpMenu(MouseEvent mouseEvent) {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("SignUpMenu.fxml"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        //TODO
+    }
+}
