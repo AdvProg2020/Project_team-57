57753e19ccd3a862ae2511b46fd615baca01b32b
sepehrkianian09 <59197271+sepehrkianian09@users.noreply.github.com>
2020-06-07 02:30:50 +0430

    Complete Phase 1 VendorProfileMenu.css

--- src/main/java/view/ProfileProcessor.java
+++ src/main/java/view/ProfileProcessor.java
@@ -1,10 +1,13 @@
 package view;
 
+import com.jfoenix.controls.JFXTextField;
 import controller.Control;
+import javafx.collections.ObservableList;
 import javafx.fxml.Initializable;
 import javafx.scene.layout.Pane;
 import model.existence.Account;
 
+import javax.swing.text.html.ImageView;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -12,17 +15,39 @@ public class ProfileProcessor implements Initializable {
     private static Account account;
 
     public Pane mainPane;
+    public JFXTextField firstName, lastName, email, brand, credit;
+    public ImageView profileImage;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        String accountType = Control.getType();
+        setStyleSheets();
+        setFields();
+    }
+
+    private void setFields() {
+        firstName.setText(account.getFirstName());
+        lastName.setText(account.getLastName());
+        email.setText(account.getEmail());
+
+        if(account.getType().equals("Vendor")) {
+            brand.setText(account.getBrand());
+        } else {
+            //TODO
+        }
+    }
+
+    public void setStyleSheets() {
+        String accountType = Control.getType(), styleSheet;
+        ObservableList<String> styleSheets = mainPane.getStylesheets();
+        styleSheets.removeAll(styleSheets);
 
         if(accountType.equals("Admin")) {
-
+            styleSheet = "CSS files\\AdminProfileMenu.css";
         } else if(accountType.equals("Vendor")) {
-
+            styleSheet = "CSS files\\VendorProfileMenu.css";
         } else {
-
+            styleSheet = "CSS files\\CustomerProfileMenu.css";
         }
+        styleSheets.add(styleSheet);
     }
 }
