53d670e3ba4f8e7d93c22e77066843e377a75609
sepehrkianian09 <59197271+sepehrkianian09@users.noreply.github.com>
2020-06-07 01:11:59 +0430

    Add ProfileMenu.fxml

--- src/main/java/view/ProfileProcessor.java
+++ src/main/java/view/ProfileProcessor.java
@@ -1,6 +1,8 @@
 package view;
 
+import controller.Control;
 import javafx.fxml.Initializable;
+import javafx.scene.layout.Pane;
 import model.existence.Account;
 
 import java.net.URL;
@@ -9,8 +11,18 @@ import java.util.ResourceBundle;
 public class ProfileProcessor implements Initializable {
     private static Account account;
 
+    public Pane mainPane;
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        String accountType = Control.getType();
 
+        if(accountType.equals("Admin")) {
+
+        } else if(accountType.equals("Vendor")) {
+
+        } else {
+
+        }
     }
 }
